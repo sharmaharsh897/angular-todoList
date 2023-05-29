95ac8ef969edb92f170cda75735b4840eda558ba
sharmaharsh897 <sharmaharsh897@gmail.com>
2023-05-29 16:55:52 +0530

    First commit

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,18 @@
+import { Component,OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit{
+
+
+  todos:Todo[];
+  constructor(){ }
+
+    ngOnInit():void{
+
+    }
+}
