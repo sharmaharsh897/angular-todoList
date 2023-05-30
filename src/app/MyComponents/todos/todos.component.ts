6e6122288dd57dd144b0cd0282b3a749896b3cdd
sharmaharsh897 <sharmaharsh897@gmail.com>
2023-05-30 10:17:46 +0530

    Added button to delete todos

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -10,9 +10,36 @@ export class TodosComponent implements OnInit{
 
 
   todos:Todo[];
-  constructor(){ }
+  constructor(){
+    this.todos=[
+       {
+        sno:1,
+        title:"title1",
+        desc:"Description1",
+        active:true
+    },
+      {
+        sno:2,
+        title:"title2",
+        desc:"Description2",
+        active:true
+    },
+      {
+          sno:3,
+          title:"title3",
+          desc:"Description3",
+          active:true
+      }
+    ]
+   }
 
     ngOnInit():void{
 
+    }
+
+    deleteTodo(todo: Todo){
+      console.log(todo);
+      const index=this.todos.indexOf(todo);
+      this.todos.splice(index,1)
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule
